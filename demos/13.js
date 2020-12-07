5422c49241c8e85873a1a517417661d4ad572d70
360BUYAD\likai254 <likai254@jd.com>
2020-12-07 15:59:36 +0800

    2020-12-7

--- demos/13.js
+++ demos/13.js
@@ -2,8 +2,10 @@ const Koa = require('koa');
 const route = require('koa-route');
 const app = new Koa();
 var cors = require('koa2-cors');
+const htmlRender = require("koa-html-render") 
 
 app.use(cors());
+app.use(htmlRender())
 
 const redirect = ctx => {
   ctx.response.redirect('/');
@@ -17,8 +19,20 @@ const myData = ctx => {
   ctx.response.body = "{name: 'likai'}";
 };
 
+const jsonp = ctx => {
+  const json = require('./config.js')
+  const temp = json+''
+  ctx.response.body = temp;
+};
+const tool = ctx => {
+  ctx.html("./index.html")
+};
+
 app.use(route.get('/', main));
 app.use(route.get('/data', myData));
+app.use(route.get('/jsonp', jsonp));
+app.use(route.get('/tool', tool));
+
 app.use(route.get('/redirect', redirect));
 
 app.use(main);
